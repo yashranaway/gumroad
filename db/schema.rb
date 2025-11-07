63bdc75c49e32e31620f6684d6e527c97db106d1
adityagarud <garudaditya079@gmail.com>
2025-11-07 23:22:20 +0530

    chore: remove foreign key from installment_plan_snapshots

--- db/schema.rb
+++ db/schema.rb
@@ -2730,5 +2730,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_14_161000) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "installment_plan_snapshots", "payment_options"
 end
