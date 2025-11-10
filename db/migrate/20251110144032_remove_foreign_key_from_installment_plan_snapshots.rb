a240109780840231d6ef609301460165b2d25ba9
adityagarud <garudaditya079@gmail.com>
2025-11-10 20:30:10 +0530

    fix

--- db/migrate/20251110144032_remove_foreign_key_from_installment_plan_snapshots.rb
+++ db/migrate/20251110144032_remove_foreign_key_from_installment_plan_snapshots.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveForeignKeyFromInstallmentPlanSnapshots < ActiveRecord::Migration[7.1]
   def change
     remove_foreign_key :installment_plan_snapshots, :payment_options
