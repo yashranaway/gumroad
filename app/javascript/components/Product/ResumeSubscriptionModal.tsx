9988daf016735174e91d80d0bc130e764aa7de5f
adityagarud <garudaditya079@gmail.com>
2026-01-06 16:58:10 +0530

    lints

--- app/javascript/components/Product/ResumeSubscriptionModal.tsx
+++ app/javascript/components/Product/ResumeSubscriptionModal.tsx
@@ -15,14 +15,14 @@ export const ResumeSubscriptionModal = ({ open, onClose, onStartNewSubscription,
     open={open}
     onClose={onClose}
     title="Resume your previous subscription?"
-    className="!shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] !border-2 !border-black"
+    className="!border-2 !border-black !shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]"
   >
     <div className="flex flex-col gap-6">
       <p className="text-lg">
         You've previously subscribed to this product. Would you like to <strong>pick up where you left off</strong>, or{" "}
         <strong>start fresh with a new subscription</strong>?
       </p>
-      <div className="flex flex-col gap-3 sm:flex-row w-full">
+      <div className="flex w-full flex-col gap-3 sm:flex-row">
         <Button className="flex-1 justify-center" onClick={onStartNewSubscription} color="black" outline>
           No, start a new subscription
         </Button>
