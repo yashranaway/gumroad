be4ab1ef47b8953ec7eb97b65f791a53c8f5595e
Aditya Garud <garudaditya079@gmail.com>
2026-03-27 21:13:13 +0530

    Assert offer code is accepted in discount short link test

--- spec/requests/product_custom_domain_short_links_spec.rb
+++ spec/requests/product_custom_domain_short_links_spec.rb
@@ -16,12 +16,15 @@ describe "Product custom domain short links", type: :request do
   end
 
   describe "GET /l/:id/:code" do
-    it "renders the product page and sets noindex header" do
+    let!(:offer_code) { create(:offer_code, products: [product], code: "SAVE10") }
+
+    it "renders the product page with the discount code applied" do
       get "/l/#{product.unique_permalink}/SAVE10", headers: { "HOST" => custom_domain.domain }
 
       expect(response).to have_http_status(:ok)
       expect(response.body).to include(product.name)
       expect(response.headers["X-Robots-Tag"]).to eq("noindex")
+      expect(response.body).to include("&quot;discount_code&quot;:{&quot;valid&quot;:true,&quot;code&quot;:&quot;SAVE10&quot;")
     end
   end
 end
